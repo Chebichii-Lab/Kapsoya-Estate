830e94f5d19e8de9362257a4a33c1ca66997833b
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-23 22:32:54 +0300

    add registration view function

--- kapsoya/views.py
+++ kapsoya/views.py
@@ -1,7 +1,23 @@
+from kapsoya.forms import SignupForm
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
 
     return render(request,'index.html')
 
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('login')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/registration_form.html', {'form': form})
