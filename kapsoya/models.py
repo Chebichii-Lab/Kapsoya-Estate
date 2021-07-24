8da937927da4609db44e1fb75c9ed31d303e4212
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-24 11:37:49 +0300

    add profile class and second migration

--- kapsoya/models.py
+++ kapsoya/models.py
@@ -9,3 +9,11 @@ class Neighbourhood(models.Model):
     neighbourhood_location = models.CharField(max_length=60,blank=False)
     occupants_count = models.IntegerField()
     admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='neighbour')
+
+#class User
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default='0')
+    neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, related_name='members', blank=True, null=True)
+    neighbourhood_name = models.CharField(max_length=60,blank=False)
+    location = models.CharField(max_length=60,blank=False)
+    bio = models.TextField()
--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -139,6 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
