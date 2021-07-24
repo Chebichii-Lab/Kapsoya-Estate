bd797f520aa192d96f5d35fa4b7f410d5ada4f11
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-24 16:06:26 +0300

    define the url for the view function

--- kapsoya/urls.py
+++ kapsoya/urls.py
@@ -11,6 +11,7 @@ urlpatterns=[
     url('login/', auth_views.LoginView.as_view(), name='login'),
     url('logout/',auth_views.LogoutView.as_view(), name='logout'),
     url('all-hoods/',views.neighbourhoods,name='neighbourhood'),
+    url('new-hood/', views.create_neighbourhood, name='new-hood'),
 ]
 
 if settings.DEBUG:
